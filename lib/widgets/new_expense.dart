1b5a1700234b08a4c24c60d832f1cb01b57b3dcf
jbnewman27 <jbarilne@nyit.edu>
2026-04-07 11:03:03 -0400

    added dispose function&button

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -13,6 +13,15 @@ class NewExpense extends StatefulWidget{
 class _NewExpenseState extends State<NewExpense>{
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+
+  @override
+  void dispose(){
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context){
     return Padding(padding: EdgeInsets.all(16),
@@ -36,6 +45,9 @@ class _NewExpenseState extends State<NewExpense>{
           ),
           ),
         Row(children: [
+           ElevatedButton(onPressed: (){
+           dispose();
+            }, child: Text("Cancel")),
           ElevatedButton(onPressed: (){
             print(_titleController.text);
             print(_amountController.text);
