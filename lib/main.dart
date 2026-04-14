1308282f49738bc88f4cf0e03173ab4c61f4806a
jbnewman27 <jbarilne@nyit.edu>
2026-04-14 12:12:55 -0400

    Started themeing

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,35 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+
+
+
+ColorScheme kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
 void main() {
-  runApp(const MaterialApp(
+  runApp( MaterialApp(
+    theme: ThemeData(
+      textTheme: ThemeData().textTheme.copyWith(
+        titleLarge: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: kColorScheme.onSecondaryContainer,
+          fontSize: 20,
+        )
+      ),
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+      ),
+      cardTheme: CardThemeData(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer,
+        )
+      )
+    ),
     home: Expenses()
   ));
 }
--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -12,7 +12,9 @@ class ExpenseItem extends StatelessWidget{
         vertical: 16),
         child: Column(
           children: [
-            Text(expense.title),
+            Text(expense.title,
+                  style: Theme.of(context).textTheme.titleLarge,
+            ),
             const SizedBox(height:4),
             Row(children: [
               Text('\$${expense.amount.toStringAsFixed(2)}'),
